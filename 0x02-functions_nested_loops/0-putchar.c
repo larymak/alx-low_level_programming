89bd071eb79863574acff1e7c9f189803fe25c6a
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-03-16 15:54:02 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
 #include "main.h"
+
+
 /**
- *main - entry point
+ * main - Entry point
  *
- *Description: prints Holberton followed by a newline
- *
- *Return: returns 0(successful)
- *
+ * Return: nothing, return void
  */
+
 int main(void)
 {
-	char holbText[9] = "Holberton";
-	int i;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(holbText[i]);
-	}
-	_putchar('\n');
-	return (0);
+return (0);
 }
