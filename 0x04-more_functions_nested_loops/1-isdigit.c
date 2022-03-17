d010a9232821ee8338eb4797db106674e3264dcb
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-03-17 23:47:55 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,11 @@
-#include "main.h
-/**
-  _isdigit - function that prints the sign of a number
-  @c: is a digit
-  Return: 1 if c is a digit, 0 otherwise.
-**/
+#include "main.h"
 
-int _isdigit(int c)
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @num: int to be checked
+ * Return: 1 if num is a digit, 0 otherwise
+ */
+int _isdigit(int num)
 {
-        if (c >= '0' && c <= '9')
-        {
-                 return (1);
-        }
-    Reiseded with iTop Screen Recorder
-                 return (0);
+	return (num >= '0' && num <= '9');
 }
