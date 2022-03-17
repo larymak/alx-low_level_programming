d32ed1936b12c3caedb9ad39d3a5e618fb9e2ab7
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-03-17 23:30:14 +0300

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h
+/**
+  _isdigit - function that prints the sign of a number
+  @c: is a digit
+  Return: 1 if c is a digit, 0 otherwise.
+**/
+
+int _isdigit(int c)
+{
+        if (c >= '0' && c <= '9')
+        {
+                 return (1);
+        }
+    Reiseded with iTop Screen Recorder
+                 return (0);
+}
