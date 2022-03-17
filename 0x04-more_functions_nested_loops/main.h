3d6fdf7293a0f1dc637f4ab79062916ccf116d14
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-03-17 23:10:27 +0300

    Create main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
