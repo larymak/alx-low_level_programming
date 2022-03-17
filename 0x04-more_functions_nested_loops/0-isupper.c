779754d8ec908469aedd21a53016f06f8407b796
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-03-17 08:43:35 +0300

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char Array[] = "_putchar\n";
+int al = 0;
+
+while (al < 9)
+{
+_putchar(Array[al]);
+al++;
+}
+
+return (0);
+
+}
