b3450f92858a64b51ec29d679aadef5ee80766d8
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-03-24 21:12:09 +0300

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,42 +8,35 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, pi, c;
+	int j, k, l;
 
 	if (size <= 0)
 		printf("\n");
 	else
 	{
-		for (i = 9, pi = 0; i < size; i += 10)
-		{	printf("%.8x: ", pi);
-				for (c = pi; c <= i ; c++)
-				{	printf("%.2x", b[c]);
-					if (c % 2 == 1)
-						printf(" ");	}
-				for (c = pi; c <= i ; c++)
-				{
-					if (b[c] < 32)
-						printf(".");
-					else
-						printf("%c", b[c]);	}
-					printf("\n");
-					pi = i + 1;	}
-		if (pi < size - 1)
-		{	i -= 9;
-			printf("%.8x: ", i);
-			for (c = i; c < i + 10; c++)
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
 			{
-				if (c < size)
-					printf("%.2x", b[c]);
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
 				else
 					printf("  ");
-				if (c % 2 == 1)
-					printf(" ");	}
-			for (; i < size ; i++)
-			{
-				if (b[i] < 32)
-					printf(".");
-				else
-					printf("%c", b[i]);
 			}
-			printf("\n");	}	}	}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
+	}
+}
