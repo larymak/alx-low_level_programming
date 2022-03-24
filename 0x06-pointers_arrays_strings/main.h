43811a78075a18c0fc9a2616c344568a90d95bcf
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-03-24 09:56:33 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,12 @@
-#ifndef main
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int _putchar(char c);
+void putchar_many(int n);
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -8,6 +16,9 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
-#define main
-#endif
+#endif /* MAIN_H */
