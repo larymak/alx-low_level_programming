bbc0e8301ae59667830ee3ab1c40baf09c4c63df
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-03-28 22:40:12 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,6 @@
-#ifndef main
-#include <stdlib.h>
-
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
@@ -11,6 +8,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#define main
-#endif
