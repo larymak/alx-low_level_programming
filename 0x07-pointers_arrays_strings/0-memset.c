df496f836ac7b417ccc4820c2f44e9a18989560c
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-03-28 21:33:31 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * @char *_memcpy - copies n bytes from memory area src to memory area dest
- * @dest: pointer to destination memory area
- * @src: pointer to source memory area
- * @n: number of bytes to copy
- * Return: pointer to destination memory area
+ * _memset - fills string with constant char b upto n bytes
+ * @s: input pointer to string
+ * @b: constant char
+ * @n: number of bytes
+ * Return: pointer to s string
  */
 
 char *_memset(char *s, char b, unsigned int n)
