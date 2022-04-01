69e0f97e81305d22b2fa4480a42bb4cece59788e
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-04-01 17:05:48 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,10 @@
  * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char **argv)
 {
-    if (argc > 0)
-        printf("%s\n", argv[0]);
-    return (0);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
 }
