cca211e8254a29753073b580f96a4a1fafe0ac6e
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-04-05 23:33:33 +0300

    create array dynamically

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,32 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and initializes it with
- * a specific char
- * @size: size of array
- * @c: character to initialize with
- *
- * Return: Pointer
+ *create_array - allocate an array.
+ *@size: size of arrray.
+ *@c: char.
+ *Return: pointer to char if success.
  */
+
 char *create_array(unsigned int size, char c)
 {
+	char *tmp = malloc(sizeof(char) * size);
 	unsigned int i;
-	char *s;
 
-	if (size <= 0)
-		return (0);
-
-	s = malloc(sizeof(char) * size);
-
-	if (s == 0)
-		return (0);
-
+	if (size == 0 || tmp == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
-		*(s + i) = c;
-
-	*(s + i) = '\0';
-
-	return (s);
+		tmp[i] = c;
+	return (tmp);
 }
