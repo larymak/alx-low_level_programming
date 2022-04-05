5ef68e5d39e52b53f79afa75076b790efa6d13ee
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-04-05 23:32:30 +0300

    Header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,3 @@
-#ifndef _FUNCTIONS_H
-#define _FUNCTIONS_H
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -8,4 +6,3 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-#endif
