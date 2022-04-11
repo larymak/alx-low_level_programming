c0045bf0af35c77c244a9df38214b813ff1f1def
Hillary Nyakundi <63947040+larymak@users.noreply.github.com>
2022-04-11 23:57:58 +0300

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,24 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef STRUCTS
+#define STRUCTS
 /**
- * struct dog - Dog description.
- * @name: Dog name.
- * @age: Dog age.
- * @owner: Dog's owner.
- **/
-
+ * struct dog - description for a pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} description;
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
+} dog_t;
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
 int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
